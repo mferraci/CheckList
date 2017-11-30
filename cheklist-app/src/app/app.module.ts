62dc7b0907964c4202bf937f17beeab386475c4a
mferraci <mferraci@gmail.com>
2017-11-30 23:19:37 +0100

    FIrst UI Draft

--- cheklist-app/src/app/app.module.ts
+++ cheklist-app/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome';
 
 
 import { AppComponent } from './app.component';
+import { HorizontalMenuComponent } from './horizontal-menu/horizontal-menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HorizontalMenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Angular2FontawesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
